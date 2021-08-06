24249a9a7f744e86ebf53789b369595e8ebceb0c
Julius Osokinas <julius.osokinas@onfido.com>
2021-08-06 15:36:54 +0100

    feat: load css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 const modules = {
-  A: import('https://josokinas.github.io/micro-frontend-a/index.js'),
-  B: import('https://josokinas.github.io/micro-frontend-b/index.js'),
+  A: {
+    js: 'https://josokinas.github.io/micro-frontend-a/index.js',
+    css: 'https://josokinas.github.io/micro-frontend-a/index.css',
+  },
+  B: {
+    js: 'https://josokinas.github.io/micro-frontend-b/index.js',
+    css: 'https://josokinas.github.io/micro-frontend-b/index.css',
+  },
 };
 
-Object.entries(modules).forEach(async ([name, importMfe]) => {
-  await importMfe;
+Object.entries(modules).forEach(async ([name, imports]) => {
+  await import(imports.js);
 
-  window.MFE[name].root.id = name;
+  window.MFE[name].root.id = `${name}-JS`;
   document.body.appendChild(window.MFE[name].root);
 
   window.MFE[name].render();
+
+  const link = document.createElement('link');
+  link.id = `${name}-CSS`;
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = imports.css;
+
+  const [head] = document.getElementsByTagName('HEAD');
+  head.appendChild(link);
 });
