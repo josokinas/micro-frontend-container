ddfbf1f0e056e025ce3443b8f94d819dd50d2d8e
Julius Osokinas <julius.osokinas@onfido.com>
2021-08-06 14:55:38 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './styles.scss';
+
+const modules = {
+  A: import('https://josokinas.github.io/micro-frontend-a/index.js'),
+  B: import('https://josokinas.github.io/micro-frontend-b/index.js'),
+};
+
+Object.entries(modules).forEach(async ([name, importMfe]) => {
+  await importMfe;
+
+  window.MFE[name].root.id = name;
+  document.body.appendChild(window.MFE[name].root);
+
+  window.MFE[name].render();
+});
