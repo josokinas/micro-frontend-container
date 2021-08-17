afd70a774f33b5c49dc8bbdd2239e2518ace15e4
Julius Osokinas <julius.osokinas@onfido.com>
2021-08-17 17:19:02 +0100

    feat: set current location for deployment

--- src/index.js
+++ src/index.js
@@ -9,5 +9,8 @@ Object.entries(modules).forEach(async ([name, source]) => {
   window.MFE[name].root.id = name;
   document.body.appendChild(window.MFE[name].root);
 
-  window.MFE[name].render({ testProp: 'testValue' });
+  window.MFE[name].render({
+    currentLocation: process.env.PUBLIC_URL,
+    testProp: 'testValue',
+  });
 });
